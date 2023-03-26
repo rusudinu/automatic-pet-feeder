cbb8745bc9a0fb90d4ef0667eeaeb02617b443ae
Dinu Rusu <dinu@codingshadows.com>
2023-03-26 11:43:44 +0300

    centered the hourly feed buttons

--- lib/home.dart
+++ lib/home.dart
@@ -35,6 +35,7 @@ class _HomeState extends State<Home> {
           ],
         ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton.icon(
               icon: const Icon(Icons.timer),
